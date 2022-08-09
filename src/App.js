5a0d395f3570c1113f711b7287e39b8a64c61546
HAMDI-BENABDALLAH <108347077+HAMDI-BENABDALLAH@users.noreply.github.com>
2022-08-09 16:05:33 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './componet/Navbar/Navbar';
+import Main from './componet/Navbar/Main';
+function App () {
+	return (
+		<div className="App">
+			<Navbar />
+			<Main />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/componet/Navbar/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function footer() {
+  return (
+
+        <div class="footer">
+            <p>copyright hamdi ben abdallah</p>
+
+        </div>
+    
+  )
+}
+
+export default footer
